e9189e03c7ecb5ebdcac64dbc5b6da87c8213a87
khaledsherif97 <ksherif890@gmail.com>
2024-10-05 21:43:06 +0300

    fix

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='w-25 m-auto mt-5'>
+      <h2 className=' text-primary'> Not Found </h2>
+    <Link to="home" > <button className='custom-btn fs-5 fw-semibold mx-1 py-2 fw-bold'> Home Page <i class="fa-solid fa-arrow-right ms-1"></i> </button> </Link>
+    </div>
   )
 }
 
